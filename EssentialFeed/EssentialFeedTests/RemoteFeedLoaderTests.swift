78a9e740948af1b17224fe4f1dc9c1c0e7d597c8
YourDaddy2000 <47114561+YourDaddy2000@users.noreply.github.com>
2021-08-21 21:11:41 +0300

    improve test name

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -15,7 +15,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let (sut, client) = getSUT()
         
         sut.load()
