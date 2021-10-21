f8717befd3f0fb61100cb8c944e3503ec525b539
YourDaddy2000 <47114561+YourDaddy2000@users.noreply.github.com>
2021-10-21 22:34:01 +0300

    move codable conformance from the framework-agnostic LocalFeedImage type to framework-specific CodableFeedIMage type

--- EssentialFeed/EssentialFeed/API/LocalFeedImage.swift
+++ EssentialFeed/EssentialFeed/API/LocalFeedImage.swift
@@ -5,11 +5,11 @@
 //  Created by Roman Bozhenko on 12.10.2021.
 //
 
-public struct LocalFeedImage: Equatable, Codable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let url: URL
+public struct LocalFeedImage: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
     
     public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
