040f29aa2011d5ad76b4ff782e90c042c3d21f71
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2021-12-27 14:49:23 +0200

    Add FeedImagePresenter holding FeedImage presentation logic (including image loading state)

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -33,13 +33,14 @@ private final class FeedViewAdapter: FeedViewProtocol {
     
     func display(viewModel: FeedViewModel) {
         controller?.tableModel = viewModel.feed.map { model in
-            FeedImageCellController(
-                viewModel: FeedImageViewModel(
-                    model: model,
-                    imageLoader: loader,
-                    imageTransformer: UIImage.init
-                )
-            )
+            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: loader)
+            let view = FeedImageCellController(delegate: adapter)
+            
+            adapter.presenter = FeedImagePresenter(
+                view: WeakRefVirtualProxy(view),
+                imageTransformer: UIImage.init)
+            
+            return view
         }
     }
 }
@@ -55,6 +56,12 @@ final class WeakRefVirtualProxy<T: AnyObject> {
 extension WeakRefVirtualProxy: FeedLoadingViewProtocol where T: FeedLoadingViewProtocol {
     func display(viewModel: FeedLoadingViewModel) {
         object?.display(viewModel: viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageViewProtocol where T: FeedImageViewProtocol, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
     }
 }
 
@@ -77,5 +84,36 @@ final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
                 presenter?.didEndLoadingFeed(with: error)
             }
         }
+    }
+}
+
+final class FeedImageDataLoaderPresentationAdapter<View: FeedImageViewProtocol, Image>: FeedImageCellControllerDelegate where View.Image == Image {
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private var task: FeedImageDataLoaderTask?
+    
+    var presenter: FeedImagePresenter<View, Image>?
+    
+    internal init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+        
+        let model = self.model
+        task = imageLoader.loadImageData(from: model.url, completion: { [weak presenter] result in
+            switch result {
+            case .success(let data):
+                presenter?.didFinishLoadingImageData(with: data, for: model)
+            case .failure(let error):
+                presenter?.didFinishLoadingImageData(with: error, for: model)
+            }
+        })
+    }
+    
+    func didCancelImageRequest() {
+        task?.cancel()
     }
 }
