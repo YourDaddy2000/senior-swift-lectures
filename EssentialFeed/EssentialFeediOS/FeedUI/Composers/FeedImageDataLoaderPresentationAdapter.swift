0d2e7738ec5088bf2ea3f72eab85aaabffcbed98
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-01-02 19:16:22 +0200

    move FeedImageDataLoaderPresentationAdapter to a separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedImageDataLoaderPresentationAdapter.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageDataLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Roman Bozhenko on 02.01.2022.
+//
+
+import EssentialFeed
+
+final class FeedImageDataLoaderPresentationAdapter<View: FeedImageViewProtocol, Image>: FeedImageCellControllerDelegate where View.Image == Image {
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private var task: FeedImageDataLoaderTask?
+    
+    var presenter: FeedImagePresenter<View, Image>?
+    
+    internal init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+        
+        let model = self.model
+        task = imageLoader.loadImageData(from: model.url, completion: { [weak presenter] result in
+            switch result {
+            case .success(let data):
+                presenter?.didFinishLoadingImageData(with: data, for: model)
+            case .failure(let error):
+                presenter?.didFinishLoadingImageData(with: error, for: model)
+            }
+        })
+    }
+    
+    func didCancelImageRequest() {
+        task?.cancel()
+    }
+}
--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -78,34 +78,3 @@ final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
         }
     }
 }
-
-final class FeedImageDataLoaderPresentationAdapter<View: FeedImageViewProtocol, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-    private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private var task: FeedImageDataLoaderTask?
-    
-    var presenter: FeedImagePresenter<View, Image>?
-    
-    internal init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-    
-    func didRequestImage() {
-        presenter?.didStartLoadingImageData(for: model)
-        
-        let model = self.model
-        task = imageLoader.loadImageData(from: model.url, completion: { [weak presenter] result in
-            switch result {
-            case .success(let data):
-                presenter?.didFinishLoadingImageData(with: data, for: model)
-            case .failure(let error):
-                presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        })
-    }
-    
-    func didCancelImageRequest() {
-        task?.cancel()
-    }
-}
