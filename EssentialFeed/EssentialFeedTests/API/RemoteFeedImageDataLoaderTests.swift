73c261d47ffc3c401ae188610c82715b3f26fcec
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-01-23 13:09:50 +0200

    RemoteFeedImageDataLoader does not perform any URL request, RemoteFeedImageDataLoader requests data from URL on `loadImageDataFromURL`

---
+++ EssentialFeed/EssentialFeedTests/API/RemoteFeedImageDataLoaderTests.swift
@@ -0,0 +1,54 @@
+//
+//  RemoteFeedImageDataLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Roman Bozhenko on 23.01.2022.
+//
+
+import XCTest
+import EssentialFeed
+
+final class RemoteFeedImageDataLoader {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
+    }
+}
+
+
+class RemoteFeedImageDataLoaderTests: XCTestCase {
+
+    func test_init_doesNotMessageStore() {
+        let (_, spy) = makeSUT()
+        XCTAssertEqual(spy.requestedURLs, [])
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let (sut, spy) = makeSUT()
+        let url = anyURL()
+        sut.loadImageData(from: url) { _ in }
+        XCTAssertEqual(spy.requestedURLs, [url])
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT() -> (RemoteFeedImageDataLoader, HTTPClientSpy) {
+        let spy = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: spy)
+        trackForMemoryLeaks(spy)
+        trackForMemoryLeaks(sut)
+        return (sut, spy)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (Response) -> Void) {
+            requestedURLs.append(url)
+        }
+    }
+}
