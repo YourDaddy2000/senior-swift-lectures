38931483bea7c98f9c991901ed19b0bc0ac6ee28
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-01-23 16:25:19 +0200

    Move RemoteFeedImageDataLoader to production

--- EssentialFeed/EssentialFeedTests/API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/API/RemoteFeedImageDataLoaderTests.swift
@@ -6,63 +6,7 @@
 //
 
 import XCTest
-import EssentialFeed
-
-final class RemoteFeedImageDataLoader {
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-    
-    private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        var wrapped: HTTPClientTask?
-        
-        init(completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPTaskWrapper(completion: completion)
-        
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard let _ = self else { return }
-            switch result {
-            case .failure(let error):
-                task.complete(with: .failure(error))
-            case .success((let data, let response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            }
-        }
-        
-        return task
-    }
-}
-
+@testable import EssentialFeed
 
 class RemoteFeedImageDataLoaderTests: XCTestCase {
 
