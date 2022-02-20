4c0f22696432026500fc37f811208a4d4d35d562
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-02-20 18:49:57 +0200

    add UI Target for running high-level AcceptanceTests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,21 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Roman Bozhenko on 20.02.2022.
+//
+
+import XCTest
+
+class EssentialAppUIAcceptanceTests: XCTestCase {
+
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+    }
+    
+}
