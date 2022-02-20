eb24b9c13633500881fdda0391f119509ed5c8a7
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-02-20 17:46:40 +0200

    Create saveIgnoringResult method to clarify intent

--- EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -22,10 +22,16 @@ public final class FeedLoaderCacheDecorator: FeedLoader {
             guard let self = self else { return }
             
             completion(result.map({ feed in
-                self.cache.save(feed) { _ in }
+                self.cache.saveIgnoringResult(feed)
                 
                 return feed
             }))
         }
+    }
+}
+
+private extension FeedCache {
+    func saveIgnoringResult(_ feed: [FeedImage]) {
+        save(feed) { _ in }
     }
 }
