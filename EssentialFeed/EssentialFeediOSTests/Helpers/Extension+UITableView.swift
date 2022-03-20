829f160055f52b91c7453b14eff2a8ed9ccd31cc
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-03-20 17:26:24 +0200

    Fix table header resizing for multi-line label

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/Extension+UITableView.swift
@@ -0,0 +1,23 @@
+//
+//  Extension+UITableView.swift
+//  EssentialFeediOSTests
+//
+//  Created by Roman Bozhenko on 20.03.2022.
+//
+
+import UIKit
+
+extension UITableView {
+    func sizeTableHeaderToFit() {
+        guard let header = tableHeaderView else { return }
+        
+        let size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        
+        let needsFrameUpdate = header.frame.height != size.height
+        
+        if needsFrameUpdate {
+            header.frame.size.height = size.height
+            tableHeaderView = header
+        }
+    }
+}
