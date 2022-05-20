4818d45c833a5b8433607e17f9373d4948d5542c
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-05-20 14:16:31 +0300

    Remove unused FeedPresenter logic

--- EssentialFeed/EssentialFeed/FeedPresentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedPresenter.swift
@@ -5,25 +5,7 @@
 //  Created by Roman Bozhenko on 15.01.2022.
 //
 
-public protocol FeedViewProtocol {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
-    private let errorView: ResourceErrorViewProtocol
-    private let loadingView: ResourceLoadingViewProtocol
-    private let feedView: FeedViewProtocol
-    
-    public init(
-        feedView: FeedViewProtocol,
-        loadingView: ResourceLoadingViewProtocol,
-        errorView: ResourceErrorViewProtocol
-    ) {
-        self.feedView = feedView
-        self.loadingView = loadingView
-        self.errorView = errorView
-    }
-    
     public static var title: String {
         NSLocalizedString(
             "feed_view_title",
@@ -38,21 +20,6 @@ public final class FeedPresenter {
             tableName: "Shared",
             bundle: Bundle(for: Self.self),
             comment: "Error message displayed when we can't load the image feed from the server")
-    }
-    
-    public func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-    
-    public func didFinishLoading(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-    
-    public func didFinishLoading(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
--- EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresentationTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresentationTests.swift
@@ -10,43 +10,15 @@ import XCTest
 
 class FeedPresentationTests: XCTestCase {
     
-    func test_init_doesNotMessageView() {
-        let (_, spy) = makeSUT()
-        XCTAssertTrue(spy.messages.isEmpty)
-    }
-    
-    func test_didStartLoadingFeed_displaysNoErrorMessage() {
-        let (sut, view) = makeSUT()
-        
-        sut.didStartLoadingFeed()
-        XCTAssertEqual(view.messages, [
-            .display(errorMessage: .none),
-            .display(isLoading: true)
-        ])
-    }
-
-    func test_didFinishLoadingFeed_displaysFeedAndStopsLoading() {
-        let (sut, view) = makeSUT()
-        let feed = uniqueImageFeed.models
-        sut.didFinishLoading(with: feed)
-        XCTAssertEqual(view.messages, [
-            .display(feed: feed),
-            .display(isLoading: false)
-        ])
-    }
-    
-    func test_didFinishLoadingFeedWithError_displaysLocalizedErrorMessageAndStopsLoading() {
-        let (sut, view) = makeSUT()
-        
-        sut.didFinishLoading(with: anyError())
-        XCTAssertEqual(view.messages, [
-            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR", table: "Shared")),
-            .display(isLoading: false)
-        ])
-    }
-    
     func test_title_isLocalized() {
         XCTAssertEqual(FeedPresenter.title, localized("feed_view_title"))
+    }
+    
+    func test_map_createsViewModel() {
+        let feed = uniqueImageFeed.models
+        let viewModel = FeedPresenter.map(feed)
+        
+        XCTAssertEqual(viewModel.feed, feed)
     }
     
     //MARK: - Helpers
@@ -59,36 +31,5 @@ class FeedPresentationTests: XCTestCase {
         }
         
         return value
-    }
-    
-    private func makeSUT() -> (FeedPresenter, ViewSpy) {
-        let viewSpy = ViewSpy()
-        let presenter = FeedPresenter(feedView: viewSpy, loadingView: viewSpy, errorView: viewSpy)
-        trackForMemoryLeaks(viewSpy)
-        trackForMemoryLeaks(presenter)
-        
-        return (presenter, viewSpy)
-    }
-    
-    private class ViewSpy: ResourceErrorViewProtocol, ResourceLoadingViewProtocol, FeedViewProtocol {
-        enum Messages: Equatable {
-            case display(errorMessage: String?)
-            case display(isLoading: Bool?)
-            case display(feed: [FeedImage])
-        }
-        
-        var messages = [Messages]()
-        
-        func display(_ viewModel: ResourceErrorViewModel) {
-            messages.append(.display(errorMessage: viewModel.message))
-        }
-        
-        func display(_ viewModel: ResourceLoadingViewModel) {
-            messages.append(.display(isLoading: viewModel.isLoading))
-        }
-        
-        func display(_ viewModel: FeedViewModel) {
-            messages.append(.display(feed: viewModel.feed))
-        }
     }
 }
