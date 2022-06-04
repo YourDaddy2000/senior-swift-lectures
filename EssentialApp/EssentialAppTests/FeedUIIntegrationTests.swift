49e379fabe0c9a31eb2773e1b7160dba78d739bb
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-06-04 15:53:43 +0300

    reorder tests

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -80,6 +80,8 @@ class FeedUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [image])
     }
     
+    // MARK: - Image View Tests
+    
     func test_feedImageView_loadsImageURLWhenVisible() {
         let image0 = makeImage(url: URL(string: "http://url-0.com")!)
         let image1 = makeImage(url: URL(string: "http://url-1.com")!)
@@ -257,20 +259,6 @@ class FeedUIIntegrationTests: XCTestCase {
         XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
     }
     
-    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
-        let (sut, loader) = makeSUT()
-        sut.loadViewIfNeeded()
-        
-        let exp = expectation(description: "Wait for background queue")
-        
-        DispatchQueue.global().async {
-            loader.completeFeedLoading(at: 0)
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1)
-    }
-    
     func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
         let (sut, loader) = makeSUT()
         
@@ -325,6 +313,20 @@ class FeedUIIntegrationTests: XCTestCase {
         
         sut.errorView.simulateTap()
         XCTAssertEqual(sut.errorMessage, nil)
+    }
+    
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        let exp = expectation(description: "Wait for background queue")
+        
+        DispatchQueue.global().async {
+            loader.completeFeedLoading(at: 0)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
     }
     
     //MARK: - Helpers
