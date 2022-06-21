0c48adc971ebd31e8949f5ef089de550d401fb6f
Roman Bozhenko <47114561+YourDaddy2000@users.noreply.github.com>
2022-06-21 15:04:59 +0300

    Propagate willDisplayCell delegate method calls

--- EssentialFeed/EssentialFeediOS/SharedUI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/SharedUI/Controllers/ListViewController.swift
@@ -59,6 +59,11 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         dl?.tableView?(tableView, didSelectRowAt: indexPath)
     }
     
+    public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let dl = cellController(at: indexPath)?.delegate
+        dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+    
     public func display(_ viewModel: EssentialFeed.ResourceLoadingViewModel) {
         viewModel.isLoading ? refreshControl?.beginRefreshing() : refreshControl?.endRefreshing()
     }
